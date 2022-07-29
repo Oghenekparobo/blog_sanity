945119b8f6b9c02f9d197d8dc924de6f50911603
Oghenekparobo <classicsteph11@gmail.com>
2022-07-29 03:05:55 +0100

    implemented headlesscms using sanity studio

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import Head from "next/head";
+import { client } from "../lib/sanity";
+import Link from 'next/link'
 
-export default function Home() {
+export default function Home({ posts }) {
   return (
     <div>
       <Head>
@@ -18,47 +20,44 @@ export default function Home() {
         <div className="wrapper p-10 space-y-8  md:grid md:grid-cols-3  ">
           <div className="blog-xl_left flex flex-col justify-center items-center col-span-2 md:h-full md:w-4/5 md:border-r border-gray-300">
             <div className="content text-center  border border-gray-300 px-8 py-4 md:p-8">
-              <p className="text-gray-300">feb 10 2022</p>
-              <h1 className=" tracking-tight font-bold text-2xl md:text-4xl md:tracking-wide">
-                Update don show
-              </h1>
-              <p className="tracking-wide text-gray-400">
-                na so the matter take set oooo
-              </p>
+              <p className="text-gray-300">Headless cms</p>
+              <p>by Stephen</p>
             </div>
           </div>
 
-          <div className="blogs-contents px-4 space-y-8 md:space-y-6">
-            <div className="content text-center border border-gray-300 p-4">
-              <p className="text-gray-300">feb 10 2022</p>
-              <h1 className="text-2xl tracking-tight font-bold">
-                Update don show
-              </h1>
-              <p className="tracking-wide text-gray-400">
-                na so the matter take set oooo
-              </p>
-            </div>
-            <div className="content text-center border border-gray-300 p-4">
-              <p className="text-gray-300">feb 10 2022</p>
-              <h1 className="text-2xl tracking-tight font-bold">
-                Update don show
-              </h1>
-              <p className="tracking-wide text-gray-400">
-                na so the matter take set oooo
-              </p>
-            </div>
-            <div className="content text-center border border-gray-300 p-4">
-              <p className="text-gray-300">feb 10 2022</p>
-              <h1 className="text-2xl tracking-tight font-bold">
-                Update don show
-              </h1>
-              <p className="tracking-wide text-gray-400">
-                na so the matter take set oooo
-              </p>
-            </div>
+          <div className="blogs-contents px-4 space-y-8 md:space-y-6 cursor-pointer">
+            {posts.map((post, index) => (
+              <Link key={index} as={`posts/${post.slug}`} href='posts/[slug]'>
+               <div
+                className="content text-center border border-gray-300 p-4"
+                
+              >
+                <p className="text-gray-300">
+                  {new Date(post.publishedAt).toDateString().slice(4)}
+                </p>
+                <h1 className="text-2xl tracking-tight font-bold">
+                  {post.title}
+                </h1>
+              </div></Link>
+             
+            ))}
           </div>
         </div>
       </section>
     </div>
   );
+}
+
+export async function getStaticProps() {
+  const posts =
+    await client.fetch(`*[_type == "post"] | order(publishedAt desc){
+    _id,
+    title,
+    publishedAt,
+    'slug': slug.current,
+  }`);
+
+  return {
+    props: { posts },
+  };
 }
